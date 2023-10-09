7094d5dc4a25f9ffbee8e2c4941e63a9c52106f1
kimcorros <45932930+kimcorros@users.noreply.github.com>
2023-10-09 10:26:24 +0000

    Build: (348df71) Remove links

--- layouts/default.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="flex flex-col min-h-screen">
-    <AppHeader class="sticky top-0" />
-    <div class="relative flex flex-col flex-1 h-full">
-      <slot />
-    </div>
-  </div>
-</template>
