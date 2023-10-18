24b8f11bc28a9cda7f1d4502e643c30d2dd394c3
Kim Francis Corros <corros121@gmail.com>
2023-10-18 14:13:19 +0800

    Update contact form

--- components/Section/Contact.vue
+++ components/Section/Contact.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-black text-white py-24">
-    <form class="container mx-auto flex flex-col items-center">
+    <form ref="formRef" class="container mx-auto flex flex-col items-center" @submit.prevent="sendMail">
       <p class="text-accent uppercase leading-[2px] text-xl font-semibold mb-4">
         CONTACT ME
       </p>
@@ -32,17 +32,21 @@
         </div>
         <div class="flex flex-col space-4">
           <div class="grid grid-cols-2 gap-x-6">
-            <FormInputText v-model="formData.name" placeholder="Name" class="w-full" />
-            <FormInputText v-model="formData.email" placeholder="Email" class="w-full" />
+            <FormInputText v-model="formData.name" placeholder="Name" class="w-full" name="from_name" />
+            <FormInputText v-model="formData.email" placeholder="Email" class="w-full" name="reply_to" />
           </div>
-          <FormInputText v-model="formData.subject" placeholder="Subject" class="w-full" />
-          <FormInputTextarea v-model="formData.message" placeholder="Message" class="w-full !mb-0" />
+          <FormInputText v-model="formData.subject" placeholder="Subject" class="w-full" name="subject" />
+          <FormInputTextarea v-model="formData.message" placeholder="Message" class="w-full !mb-0" name="message"/>
         </div>
+      </div>
+      <div v-if="showMessage" class="grid lg:grid-cols-2 gap-x-8 w-full mt-8 text-sm text-accent">
+        <div></div>
+        <p>Your message was sent!</p>
       </div>
       <div class="grid lg:grid-cols-2 gap-x-8 w-full mt-8">
         <div></div>
-        <Button to="/" class="w-full lg:max-w-fit mb-4 lg:mb-0">
-          Submit
+        <Button type="submit" class="w-full lg:max-w-fit mb-4 lg:mb-0" :disabled="loading">
+          {{ loading ? 'Sending' : 'Submit' }}
         </Button>
       </div>
     </form>
@@ -50,11 +54,34 @@
 </template>
 
 <script setup lang="ts">
-// const agreeTerms = ref<boolean>(true)
+import emailjs from '@emailjs/browser'
+
 const formData = reactive({
   name: '',
   email: '',
   subject: '',
   message: '',
 })
+const loading = ref<boolean>(false)
+const showMessage = ref<boolean>(false)
+
+const formRef = ref(null)
+
+const sendMail = () => {
+  loading.value = true
+  emailjs.sendForm('service_edek6x4', 'template_7zw6pfs', formRef.value as any, '6hBuRz84m27eTckvb')
+    .then((result) => {
+      loading.value = false
+      showMessage.value = true
+
+      formData.name = ''
+      formData.email = ''
+      formData.subject = ''
+      formData.message = ''
+
+      setTimeout(() => { showMessage.value = false }, 3000)
+    }, (error) => {
+      console.log('FAILED...', error.text);
+    })
+}
 </script>
